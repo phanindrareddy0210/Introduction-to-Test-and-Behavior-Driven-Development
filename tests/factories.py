71ce4b33c873e5269b4985b815c503915852dc69
phanindrareddy0210 <163656921+phanindrareddy0210@users.noreply.github.com>
2025-03-20 12:46:34 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,1 +1,10 @@
+from factory import Factory, Faker
 
+class ProductFactory(Factory):
+    class Meta:
+        model = dict
+
+    id = Faker('random_int', min=1, max=9999)
+    name = Faker('word')
+    category = Faker('word')
+    available = Faker('boolean')
